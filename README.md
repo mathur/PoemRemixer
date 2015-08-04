ee26262726ea7dfa4d3f58c1de0a0f2a1832b62f
Rohan Mathur <rohanmathur34@gmail.com>
2015-08-04 18:19:19 -0400

    Initial commit
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

---
+++ README.md
@@ -0,0 +1,18 @@
+Poem Remixer
+============
+
+Enter a poem to get a remixed version of the poem with the specific number of lines you specify!
+
+Under the hood, this generates Markov Chains based on your input and spits back lines that should in theory sound good together.
+
+Super basic, and simply an experiment with Markov Chains!
+
+Some cool poems to try:
+
+http://www.poetry-archive.com/b/cradle_song.html
+
+http://www.poetry-archive.com/c/bessies_song_to_her_doll.html
+
+http://www.poetry-archive.com/c/my_fancy.html
+
+http://www.poetry-archive.com/c/the_white_rabbits_verses.html
---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+
+from flask import Flask, url_for, redirect, request, render_template
+from pymarkovchain import MarkovChain
+
+PORT = int(os.environ.get('PORT', 5000))
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/poem', methods=['POST'])
+def poem():
+    story = str(request.form['story'])
+    lines = int(request.form['lines'])
+
+    if not story:
+        return redirect(url_for('index'))
+
+    mc = MarkovChain()
+    mc.generateDatabase(story)
+
+    result = []
+    for line in range(0, lines):
+        new_line = mc.generateString()
+        if new_line not in result:
+            result.append(new_line)
+
+    return render_template('poem.html', result=result, story=story)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=PORT)
